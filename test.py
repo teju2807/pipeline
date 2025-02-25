89e30824fa4579f1181d6ace9fa5abc3364ff9d9
NVT-L-0392\User <tejal.phadke@autoplant.in>
2025-02-25 11:33:51 +0530

    new commit

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import pymysql
-conn = pymysql.connect(host='10.0.1.168', user='your_user', password='your_password', database='your_db')
+from urllib.parse import quote
+
+encoded_password = quote('Aut0t@t321')
+conn = pymysql.connect(host='10.0.1.168', user='apdev', password=encoded_password, database='ap_widgets')
 print("Connected successfully")
 conn.close()
