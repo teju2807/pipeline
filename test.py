8e7eabb02cd33cdb8c43a8876702bc21e7bbc2c8
NVT-L-0392\User <tejal.phadke@autoplant.in>
2025-02-25 11:56:28 +0530

    new commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pymysql
 from urllib.parse import quote
 
-encoded_password = quote('Aut0t@t321')
-conn = pymysql.connect(host='10.0.1.168', user='apdev', password=encoded_password, database='ap_widgets')
+# encoded_password = quote('Aut0t@t321')
+conn = pymysql.connect(host='serveo.net', user='user', password='password', database='eta')
 print("Connected successfully")
 conn.close()
